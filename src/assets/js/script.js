ba8b63dee8d69cc00cf8cf945000a166b5ce00fd
Marcos Ernesto Planos Valenzuela <87524702+Dantalian5@users.noreply.github.com>
2023-08-20 16:40:44 +0200

    add script

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,1 +1,21 @@
 "use strict";
+const emailForm = document.getElementById("myForm");
+const emailInput = document.getElementById("formInput");
+const emailButton = document.getElementById("formButton");
+const emailError = document.getElementById("formError");
+const emailErrorIcon = document.getElementById("formErrorIcon");
+
+emailButton.addEventListener("click", (event) => {
+	if (!emailInput.validity.valid) {
+		emailError.innerText = "Please provide a valid email";
+		emailErrorIcon.classList.add("js-error");
+		event.preventDefault();
+		emailInput.focus();
+	}
+});
+emailInput.addEventListener("blur", (event) => {
+	if (emailInput.value == "") {
+		emailError.innerText = "";
+		emailErrorIcon.classList.remove("js-error");
+	}
+});
